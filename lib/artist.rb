4f28d6bcde60751ef23a72c95c4a9ba584febd06
Timur Mishiev <timurmishiev@gmail.com>
2020-02-20 03:01:37 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -21,6 +21,7 @@ end
   end 
 
   def add_song_by_name(name)
+    #takes in an argument of a song name, creates a new song with it and associates the song and artist
     artist = Song.new(name)
     add_song(artist)
   end
